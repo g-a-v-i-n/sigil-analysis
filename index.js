67a4141880fafd79679907da1f435b1121d00aa2
Gavin Atkinson <gvnatknsn@protonmail.com>
2020-03-01 12:55:16 -0800

    fix a bunch of stuff, add frontend

--- index.js
+++ index.js
@@ -12,7 +12,19 @@ const chunkStr = (str, size) => {
   return str.match(r);
 }
 
-const p2a = p => chunkStr(p.replace(/[\^~-]/g,''), 3)
+const p2a = p => chunkStr(p.replace(/[\^~-]/g,''), 3);
+
+
+const arrEq = (a, b) => {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
 
 const isTrue = b => b === true
 const isFalse = b => b === false
@@ -21,17 +33,7 @@ const A = 'a'
 const B = 'b'
 const C = 'c'
 const D = 'd'
-const e = 'e'
-const any = [a, b, c, e]
-
-
-
-
-const fourSided = [a, b]
-const twoSided = [e]
-const singleSided = [c, d]
-
-const anyOf = x => any.includes(x)
+const E = 'e'
 
 // index 0 [a, b, c, d, e]: key for each shape
 // index 1 [0, 1, 2, 3]: rotation count
@@ -61,107 +63,196 @@ const index = fs
     const content = fs.readFileSync(INPUT_PATH + filename, 'utf8');
     const key = path.basename(filename, '.svg');
     const ast = svgson.parseSync(content)
-    // console.log(pretty(ast))
     acc[key] = geonTileDefs[ast.children[0].children[0].attributes.id]
     return acc;
 }, {});
 
 const gtm = p => index[p]
 
-
-
-const a = {
-  isReflectiveH: ([a, b]) => {
-    if (a[1] === 0 && b[1] === 1) return true
-    if (a[1] === 1 && b[1] === 0) return true
-    if (a[1] === 2 && b[1] === 3) return true
-    if (a[1] === 3 && b[1] === 2) return true
-    return false
-  },
-  isReflectiveV: ([a, b]) => {
-    if (a[1] === 0 && b[1] === 3) return true
-    if (a[1] === 3 && b[1] === 0) return true
-    if (a[1] === 2 && b[1] === 1) return true
-    if (a[1] === 1 && b[1] === 2) return true
-    return false
-  },
-}
-
-
-const isReflectiveV = set => {
-  const [a, b] = set
-  // test if same symbol
-  if (a[0] !== b[0]) return false
-  if (a[0] === 4) {
-    if (a[1] === 0 && b[1] === 3) return true
-    if (a[1] === 3 && b[1] === 0) return true
-    if (a[1] === 2 && b[1] === 1) return true
-    if (a[1] === 1 && b[1] === 2) return true
-  }
-  if (a[3] === 2) {
-    if (a[1] === 0 && b[1] === 1) return true
-    if (a[1] === 1 && b[1] === 0) return true
-  }
-  if (a[3] === 1) return true
+const biEq = (a, b, x, y) => {
+  if (a[1] === x && b[1] === y) return true
+  if (a[1] === y && b[1] === x) return true
   return false
 }
 
-const isReflectiveH = set => {
-  const [a, b] = set
-  // test if same symbol
-  if (a[0] !== b[0]) return false
-  if (a[3] === 4) {
-    if (a[1] === 0 && b[1] === 1) return true
-    if (a[1] === 1 && b[1] === 0) return true
-    if (a[1] === 2 && b[1] === 3) return true
-    if (a[1] === 3 && b[1] === 2) return true
-  }
-  if (a[3] === 2) {
-    if (a[1] === 0 && b[1] === 1) return true
-    if (a[1] === 1 && b[1] === 0) return true
-  }
-  if (a[3] === 1) return true
+const symEq = (arr) => {
+  return arr.every(geon => geon[0] === arr[0][0])
+}
+
+const rotEq = (arr) => {
+  return arr.every(geon => geon[1] === arr[0][1])
+}
+
+const _A = {
+  isReflective: {
+    x: ([a, b]) => {
+      if (biEq(a, b, 1, 1)) return true
+      if (biEq(a, b, 2, 2)) return true
+      if (biEq(a, b, 0, 3)) return true
+      return false
+    },
+    y: ([a, b]) => {
+      if (biEq(a, b, 0, 0)) return true
+      if (biEq(a, b, 3, 3)) return true
+      if (biEq(a, b, 1, 2)) return true
+      return false
+    },
+  },
+  isRotational: quad => {
+    if (arrEq(quad, [0, 1, 2, 3])) return true
+    if (arrEq(quad, [1, 2, 3, 0])) return true
+    if (arrEq(quad, [2, 3, 0, 1])) return true
+    if (arrEq(quad, [3, 0, 1, 2])) return true
+    return false
+  },
+}
+
+const _B = {
+  isReflective: {
+    x: ([a, b]) => {
+      if (biEq(a, b, 1, 0)) return true
+      if (biEq(a, b, 2, 3)) return true
+      return false
+    },
+    y: ([a, b]) => {
+      if (biEq(a, b, 3, 0)) return true
+      if (biEq(a, b, 2, 1)) return true
+      return false
+    },
+  },
+  isRotational: quad => {
+    if (arrEq(quad, [0, 1, 2, 3])) return true
+    if (arrEq(quad, [1, 2, 3, 0])) return true
+    if (arrEq(quad, [2, 3, 0, 1])) return true
+    if (arrEq(quad, [3, 0, 1, 2])) return true
+    return false
+  },
+}
+
+
+const _C = {
+  isReflective: {
+    x: ([a, b]) => true,
+    y: ([a, b]) => true,
+  },
+  isRotational: () => true,
+}
+
+const _D = {
+  isReflective: {
+    x: () => true,
+    y: () => true,
+  },
+  isRotational: () => true,
+}
+
+const _E = {
+  isReflective: {
+    x: ([a, b]) => {
+      if (biEq(a, b, 1, 0)) return true
+      return false
+    },
+    y: ([a, b]) => {
+      if (biEq(a, b, 1, 0)) return true
+      return false
+    },
+  },
+  isRotational: quad => {
+    if (arrEq(quad, [0, 1, 2, 3])) return true
+    if (arrEq(quad, [1, 2, 3, 0])) return true
+    if (arrEq(quad, [2, 3, 0, 1])) return true
+    if (arrEq(quad, [3, 0, 1, 2])) return true
+    return false
+  },
+}
+
+const isReflectiveY = tuple => {
+
+  // Test if same symbol
+  if (!symEq(tuple)) return false
+  // Test each symbol type separately
+  if (tuple[0][0] === A) return _A.isReflective.y(tuple)
+  if (tuple[0][0] === B) return _B.isReflective.y(tuple)
+  if (tuple[0][0] === C) return _C.isReflective.y(tuple)
+  if (tuple[0][0] === D) return _D.isReflective.y(tuple)
+  if (tuple[0][0] === E) return _E.isReflective.y(tuple)
+}
+
+const isReflectiveX = tuple => {
+  // Test if same symbol
+  if (!symEq(tuple)) return false
+  // Test each symbol type separately
+  if (tuple[0][0] === A) return _A.isReflective.x(tuple)
+  if (tuple[0][0] === B) return _B.isReflective.x(tuple)
+  if (tuple[0][0] === C) return _C.isReflective.x(tuple)
+  if (tuple[0][0] === D) return _D.isReflective.x(tuple)
+  if (tuple[0][0] === E) return _E.isReflective.x(tuple)
+}
+
+const isRotational = quad => {
+  // Test if same symbol
+  if (!symEq(quad)) return false
+  // Test each symbol type separately
+  if (quad[0][0] === A) return _A.isRotational(quad)
+  if (quad[0][0] === B) return _B.isRotational(quad)
+  if (quad[0][0] === C) return _C.isRotational(quad)
+  if (quad[0][0] === D) return _D.isRotational(quad)
+  if (quad[0][0] === E) return _E.isRotational(quad)
+}
+
+const isMonolithic = (quad) => {
+  if (!symEq(quad)) return false
+  // Test each symbol type separately
+  if (quad[0][0] === A) return rotEq(quad)
+  if (quad[0][0] === B) return rotEq(quad)
+  if (quad[0][0] === C) return true
+  if (quad[0][0] === D) return true
+  if (quad[0][0] === E) return rotEq(quad)
   return false
 }
 
 const test = {
   reflection: {
-    vertical: (p) => {
+    y: (p) => {
       const tms = p2a(p).map(gtm)
       const byCol = [[tms[0], tms[2]], [tms[1], tms[3]]]
-      const res = byCol.every(isReflectiveV)
+      const res = byCol.every(isReflectiveY)
       return res
     },
-    horizontal: (p) => {
+    x: (p) => {
       const tms = p2a(p).map(gtm)
       const byRow = [[tms[0], tms[1]], [tms[2], tms[3]]]
-      const res = byRow.every(isReflectiveH)
+      const res = byRow.every(isReflectiveX)
       return res
     }
+  },
+  rotation: (p) => {
+    const tms = p2a(p).map(gtm)
+    const res = isRotational(tms)
+    return res
+  },
+  monolithic: (p) => {
+    const tms = p2a(p).map(gtm)
+    const res = isMonolithic(tms)
+    return res
   }
 }
 
-console.log(
-  test.reflection.vertical('ridlur-figbud'), // false
-  test.reflection.horizontal('ridlur-figbud') // false
-)
+const analyze = (name) => {
+  console.table({
+    patp: name,
+    yReflection: test.reflection.y(name),
+    xReflection: test.reflection.x(name),
+    rotation: test.rotation(name),
+    monolithic: test.monolithic(name),
+  })
+}
 
-console.log(
-  test.reflection.vertical('ridlur-satsyl'), // false
-  test.reflection.horizontal('ridlur-satsyl') // false
-)
 
-console.log(
-  test.reflection.vertical('donbec-satsyl'), // true
-  test.reflection.horizontal('donbec-satsyl') // true
-)
-
-console.log(
-  test.reflection.vertical('~fittyr-falref'), // true
-  test.reflection.horizontal('~fittyr-falref') // true
-)
-//~rabbur-daptuc
-console.log(
-  test.symmetry.vertical('~rabbur-daptuc'), // true
-  test.symmetry.horizontal('~rabbur-daptuc') // false
-)
+analyze('ridlur-figbud')
+analyze('ridlur-satsyl')
+analyze('fittyr-falref')
+analyze('fittyr-falref')
+analyze('rabbur-daptuc')
+analyze('todten-todten')
+analyze('fasren-dasten')
