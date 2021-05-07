f1bcd0d908eb1ea7c761f4bf8998c6fda0053c24
Gavin Atkinson <gvnatknsn@protonmail.com>
2021-05-07 11:53:36 -0700

    more cleanup

--- converttxt.js
+++ converttxt.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-const str = fs.readFileSync(__dirname + '/data/planetmarket/monrel.txt', 'utf8');
+const str = fs.readFileSync(__dirname + '/data/monrel.txt', 'utf8');
 
 const a = str.replace(/(?:\r\n|\r|\n)/g, `",`)
 const b = a.replace(/~/g, `"~`)
 
 const res = 'const monrel = [' + b + `"]; export default monrel;`
 
-fs.writeFileSync(__dirname + '/data/planetmarket/monrel.js', res);
+fs.writeFileSync(__dirname + '/data/monrel.js', res);
